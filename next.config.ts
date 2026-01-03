20fc8d748e2ff80870ce8fb44b9ef775a9598bc1
devsiten <whitecrypto122@gmail.com>
2026-01-04 00:46:14 +0100

    Add Cloudflare Pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Required for Cloudflare Pages
+  output: "standalone",
 };
 
 export default nextConfig;
